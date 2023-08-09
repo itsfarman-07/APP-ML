2c4b590f7088be96d7fa11bc50789d6a8778d215
itsfarman-07 <141563381+itsfarman-07@users.noreply.github.com>
2023-08-09 10:00:20 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+
 import sys
 from flask import Flask,request,render_template
 import numpy as np
@@ -5,8 +6,7 @@ import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
 
-sys.path.insert(0, 'src/pipeline')
-from predict_pipeline import CustomData,PredictPipeline
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application=Flask(__name__)
 
@@ -17,6 +17,7 @@ app=application
 @app.route('/')
 def index(): 
     return render_template('index.html')
+
 
 
 @app.route('/predictdata',methods=['GET','POST'])
@@ -43,6 +44,7 @@ def predict_datapoint():
         results=predict_pipeline.predict(pred_df)
         print("after Prediction")
         return render_template('home.html',results=results[0])
+    
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")    
+    app.run(host="0.0.0.0")        
